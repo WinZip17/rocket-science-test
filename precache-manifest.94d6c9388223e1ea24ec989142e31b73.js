2447102d640461eb4142adb279f8ea823694714a
WinZip <wydo@protonmail.com>
2020-07-22 23:41:13 +0300

    Updates

---
+++ precache-manifest.94d6c9388223e1ea24ec989142e31b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7cae03403cb8a7b33f134e4b576e31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee2bca1a125055cf3a47",
+    "url": "./static/css/2.f577b121.chunk.css"
+  },
+  {
+    "revision": "ee2bca1a125055cf3a47",
+    "url": "./static/js/2.06efd8a8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.06efd8a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78afdf53281685d32762",
+    "url": "./static/js/main.af4043ae.chunk.js"
+  },
+  {
+    "revision": "bbf11bce8dbe926e6733",
+    "url": "./static/js/runtime-main.50467430.js"
+  }
+]);
